23b4656cc9fd22c9b14a73dd4e871bc05c5195a9
rennysam <rennysam2002@gmail.com>
2020-08-22 15:38:39 +0530

    15:38 22/08

--- src/com/learnjava/Main.java
+++ src/com/learnjava/Main.java
@@ -1,10 +1,15 @@
 package com.learnjava;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello testing");
-        System.out.println("check commit");
+        User user = new User;
+        user.setFirstName("Renny");
+        user.setLastName("Sam");
+
+        List<User> users = new Arraylist<User>();
+        users.add(user);
     }
 }
---
+++ src/com/learnjava/User.java
@@ -0,0 +1,14 @@
+package com.learnjava;
+
+public class User {
+
+    private String firstName;
+    private String lastName;
+    public void setFirstName(String a){
+        firstName = a;
+    }
+    public void setLastName(String b){
+        lastName = b;
+    }
+
+}
