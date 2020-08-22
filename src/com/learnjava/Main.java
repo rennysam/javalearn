4afd05ba537d1fbc1a89499735f7bd31b5f9fe37
rennysam <rennysam2002@gmail.com>
2020-08-22 13:55:00 +0530

    Edit

--- src/com/learnjava/Main.java
+++ src/com/learnjava/Main.java
@@ -3,6 +3,8 @@ package com.learnjava;
 public class Main {
 
     public static void main(String[] args) {
+
         System.out.println("Hello testing");
+        System.out.println("check commit");
     }
 }
